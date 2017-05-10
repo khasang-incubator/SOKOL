2f89b1c3baf4e1c5797e5f8892dd85369234b48d
Andrey Turubchuk <antur1977@rambler.ru>
2017-05-11 00:50:07 +0300

    1. Рефакторинг (создан пакет parameteres в пакете controller, класс PagingParameters)

--- sokol/src/main/java/io/khasang/sokol/controller/parameter/PagingParameters.java
+++ sokol/src/main/java/io/khasang/sokol/controller/parameter/PagingParameters.java
@@ -1,22 +1,22 @@
-package io.khasang.sokol.pojo;
+package io.khasang.sokol.controller.parameter;
 
 /**
  * Created by AnTur on 09.05.2017.
  */
-public class PagePogo {
+public class PagingParameters {
     String pageNumber;
     String sortBy;
     String sortOrder;
     String sortOrderHeader;
 
-    public PagePogo(String pageNumber, String sortBy, String sortOrder, String sortOrderHeader) {
+    public PagingParameters(String pageNumber, String sortBy, String sortOrder, String sortOrderHeader) {
         this.pageNumber = pageNumber;
         this.sortBy = sortBy;
         this.sortOrder = sortOrder;
         this.sortOrderHeader = sortOrderHeader;
     }
 
-    public PagePogo() {
+    public PagingParameters() {
     }
 
     public String getPageNumber() {
