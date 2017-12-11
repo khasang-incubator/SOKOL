39a22c77db78b8e382fb701bb9cb13d204078a6d
Andrey Turubchuk <antur1977@rambler.ru>
2017-12-12 00:15:25 +0300

    Create simple spring boot application

---
+++ sokolboot/src/main/java/io/khasang/sokolboot/SokolbootApplication.java
@@ -0,0 +1,14 @@
+package io.khasang.sokolboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SokolbootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SokolbootApplication.class, args);
+		System.out.println("Hello");
+	}
+}
