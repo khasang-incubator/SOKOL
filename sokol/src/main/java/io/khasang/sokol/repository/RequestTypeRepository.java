c3796788e2bde0ebac7def8c5b02b851079a751c
Andrey Turubchuk <antur1977@rambler.ru>
2018-02-11 20:38:41 +0300

    Добавлен репозиторий для типов запросов

---
+++ sokol/src/main/java/io/khasang/sokol/repository/RequestTypeRepository.java
@@ -0,0 +1,7 @@
+package io.khasang.sokol.repository;
+
+import io.khasang.sokol.model.RequestType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RequestTypeRepository extends JpaRepository<RequestType, Long> {
+}
