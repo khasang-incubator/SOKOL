deb398f21315c32a546ab510fd00745729c77dd6
Andrey Turubchuk <antur1977@rambler.ru>
2018-03-15 15:43:56 +0300

    рефакторинг

---
+++ sokol/src/main/java/io/khasang/sokol/model/User.java
@@ -0,0 +1,176 @@
+/*
+ * Copyright 2016-2017 Sokol Development Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.khasang.sokol.model;
+
+//import io.khasang.sokol.validators.PasswordMatcher;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
+
+@Entity
+@Table(name = "USERS")
+//@PasswordMatcher(payload=Severity.WARNING.class)
+public class User extends AbstractBaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    int id;
+    @Size(min = 3, max = 20)
+    @NotNull
+    @Column(name = "USER_NAME")
+    private String login;
+    @Column(name = "FIO")
+    private String fio;
+    @Column(name = "EMAIL")
+    private String email;
+    @Column(name = "PASSWORD")
+    private String password;
+    @Column(name = "IS_ACTIVE")
+    private Boolean isActive;
+
+    private boolean enabled;
+
+    @ManyToOne
+    private Role role;
+
+    @ManyToOne
+    private Department department;
+
+    @Column(name = "LANGUAGE")
+    private String language;
+
+    @Version
+    private int version;
+
+    public User() {
+        super();
+    }
+
+    public void Merge(User usr) {
+        this.login = usr.login;
+        this.email = usr.email;
+        this.fio = usr.fio;
+        this.role = usr.role;
+        this.department = usr.department;
+        this.language = usr.language;
+    }
+
+/*
+    public void Merge(User usr, String password) {
+        this.Merge(usr);
+        this.setPassword(new BCryptPasswordEncoder().encode(password));
+    }
+*/
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Department getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public int getVersion() {
+        return version;
+    }
+
+    public void setVersion(int version) {
+        this.version = version;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public void setFio(String fio) {
+        this.fio = fio;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
