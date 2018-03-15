06c40f8fb397c9341148b6908232df0813223820
Andrey Turubchuk <antur1977@rambler.ru>
2018-03-15 15:41:46 +0300

    рефакторинг

---
+++ sokol/src/main/java/io/khasang/sokol/model/Department.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2016-2018 Sokol Development Team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.khasang.sokol.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "DEPARTMENTS")
+public class Department extends AbstractBaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "DEPARTMENT_ID")
+    private long id;
+
+    @Column(name = "TITLE")
+    private String title;
+
+    @Column(name = "IS_DELETED")
+    private Boolean isDeleted;
+
+
+    public Department(String title, Boolean isDeleted) {
+        this.title = title;
+        this.isDeleted = isDeleted;
+    }
+
+    public Department() {
+        this.isDeleted = false;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Boolean getDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(Boolean deleted) {
+        isDeleted = deleted;
+    }
+}
+
