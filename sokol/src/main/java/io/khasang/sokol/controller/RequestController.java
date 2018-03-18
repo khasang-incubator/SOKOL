67003ccdf1210bb56cb6cbda6f2f0778770b0431
Andrey Turubchuk <antur1977@rambler.ru>
2018-03-18 17:09:12 +0300

    рефакторинг

--- sokol/src/main/java/io/khasang/sokol/controller/RequestController.java
+++ sokol/src/main/java/io/khasang/sokol/controller/RequestController.java
@@ -4,13 +4,17 @@ package io.khasang.sokol.controller;
 import com.sun.org.apache.regexp.internal.RE;
 import io.khasang.sokol.model.Department;
 import io.khasang.sokol.model.Request;
+import io.khasang.sokol.model.RequestStatus;
 import io.khasang.sokol.model.RequestType;
 import io.khasang.sokol.repository.RequestRepository;
+import io.khasang.sokol.repository.RequestStatusRepository;
 import io.khasang.sokol.repository.RequestTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -18,7 +22,7 @@ import java.util.List;
 @Controller
 @RequestMapping(value = "/sokol/request")
 public class RequestController {
-    private static final String REQUEST_REDIRECT_TO_LIST = "redirect:/sokol/requestList/list";
+    private static final String REQUEST_REDIRECT_TO_LIST = "redirect:/sokol/request/list";
     private static final String REQUEST_LIST_HEADER_TITLE_LIST = "Запросы";
     private static final String REQUEST_LIST_HEADER_TITLE_ADD = "Добавление запроса";
     private static final String REQUEST_LIST_HEADER_TITLE_EDIT = "Редактирование запроса";
@@ -28,6 +32,9 @@ public class RequestController {
 
     @Autowired
     RequestTypeRepository requestTypeRepository;
+
+    @Autowired
+    RequestStatusRepository requestStatusRepository;
 
     @GetMapping({"/list"})
     public String requestList(Model model) {
@@ -45,6 +52,17 @@ public class RequestController {
         model.addAttribute("headerTitle", REQUEST_LIST_HEADER_TITLE_ADD);
         return "requestForm";
     }
+
+    @PostMapping("/add")
+    public String requestSubmit(@ModelAttribute Request request) {
+        RequestStatus status = requestStatusRepository.findOne(1L);
+        //RequestType requestType = requestTypeDao.getById(requestTypeId);
+        request.setStatus(status);
+        request.setVersion(1);
+        requestRepository.save(request);
+        return REQUEST_REDIRECT_TO_LIST;
+    }
+
 
 }
 
