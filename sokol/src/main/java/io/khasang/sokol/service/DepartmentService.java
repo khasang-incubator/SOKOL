7825667c22e3b0c7ed2fb656ad7db1703c7973db
bMik98 <bmik9898@gmail.com>
2018-05-03 23:51:15 +0300

    Service refactoring

--- sokol/src/main/java/io/khasang/sokol/service/DepartmentService.java
+++ sokol/src/main/java/io/khasang/sokol/service/DepartmentService.java
@@ -1,10 +1,31 @@
 package io.khasang.sokol.service;
 
 import io.khasang.sokol.model.Department;
+import io.khasang.sokol.repository.DepartmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
-public interface DepartmentService {
-    List<Department> getAll();
-    void departmentDelete(long id);
+@Service
+public class DepartmentService {
+
+    private DepartmentRepository departmentRepository;
+
+    @Autowired
+    public DepartmentService(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
+    public List<Department> getAll() {
+        return departmentRepository.findAll();
+    }
+
+    public void departmentDelete(long id) {
+        Department department = departmentRepository.getOne(id);
+        department.setDeleted(true);
+        department.setUpdatedDate(new Date());
+        departmentRepository.save(department);
+    }
 }
--- sokol/src/main/java/io/khasang/sokol/service/RequestTypeService.java
+++ sokol/src/main/java/io/khasang/sokol/service/RequestTypeService.java
@@ -1,10 +1,33 @@
 package io.khasang.sokol.service;
 
-
 import io.khasang.sokol.model.RequestType;
+import io.khasang.sokol.repository.RequestTypeRepository;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
 import java.util.List;
 
-public interface RequestTypeService {
-    List<RequestType> getAll();
-    void requestTypeDelete(long id);
+@Service
+@NoArgsConstructor
+public class RequestTypeService {
+
+    private RequestTypeRepository requestTypeRepository;
+
+    @Autowired
+    public RequestTypeService(RequestTypeRepository requestTypeRepository) {
+        this.requestTypeRepository = requestTypeRepository;
+    }
+
+    public List<RequestType> getAll() {
+        return requestTypeRepository.findAll();
+    }
+
+    public void requestTypeDelete(long id) {
+        RequestType requestType = requestTypeRepository.getOne(id);
+        requestType.setDeleted(true);
+        requestType.setUpdatedDate(new Date());
+        requestTypeRepository.save(requestType);
+    }
 }
