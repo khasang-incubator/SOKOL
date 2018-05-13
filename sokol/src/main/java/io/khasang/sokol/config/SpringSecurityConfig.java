c464cd36e39540e1a291313e5239b7533e510c81
bMik98 <bmik9898@gmail.com>
2018-05-13 21:39:40 +0300

    security config linked to RoleService correction

--- sokol/src/main/java/io/khasang/sokol/config/SpringSecurityConfig.java
+++ sokol/src/main/java/io/khasang/sokol/config/SpringSecurityConfig.java
@@ -42,8 +42,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
 
         auth.inMemoryAuthentication()
-                .withUser("user").password("password").roles("USER")
+                .withUser("user").password("password").roles(RoleService.USER_NAME)
                 .and()
-                .withUser("admin").password("password").roles("ADMIN");
+                .withUser("admin").password("password").roles(RoleService.ADMIN_NAME);
     }
 }
