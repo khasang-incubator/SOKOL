ddbb5a3ade5692e18158da94daa6f713cb6de097
Andrey Turubchuk <antur1977@rambler.ru>
2018-05-27 19:45:56 +0300

    add UserService

---
+++ sokol/src/main/java/io/khasang/sokol/service/UserService.java
@@ -0,0 +1,42 @@
+package io.khasang.sokol.service;
+
+import com.google.common.collect.ImmutableList;
+import io.khasang.sokol.model.Roles2;
+import io.khasang.sokol.model.User;
+import io.khasang.sokol.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import javax.validation.constraints.NotNull;
+
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @PostConstruct
+    public void init() {
+        if (!(userRepository.findByUsername("username") == null)) {
+            userRepository.save(User.builder()
+                    .username("username7")
+                    .password("password7")
+                    .authorities(ImmutableList.of(Roles2.USER))
+                    .accountNonExpired(true)
+                    .accountNonLocked(true)
+                    .credentialsNonExpired(true)
+                    .enabled(true)
+                    .build()
+            );
+        }
+    }
+
+    public UserDetails loadUserByUsername(@NotNull String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+}
