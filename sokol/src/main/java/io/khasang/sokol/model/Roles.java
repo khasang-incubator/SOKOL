1d7adb23df2a5d37164590e246e4f52b14956327
Andrey Turubchuk <antur1977@rambler.ru>
2018-09-28 08:45:47 +0300

    delete model roles

--- sokol/src/main/java/io/khasang/sokol/model/Roles.java
+++ sokol/src/main/java/io/khasang/sokol/model/Roles.java
@@ -2,7 +2,8 @@ package io.khasang.sokol.model;
 
 import org.springframework.security.core.GrantedAuthority;
 
-public enum Roles2 implements GrantedAuthority {
+public enum Roles implements GrantedAuthority {
+    ADMIN,
     USER;
 
     @Override
