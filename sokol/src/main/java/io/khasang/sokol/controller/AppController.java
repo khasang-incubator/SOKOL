8850d813cdeff048d0921a70450efcc68c35fcc7
Andrey Turubchuk <antur1977@rambler.ru>
2018-11-02 18:58:29 +0300

    Исправлен logout

--- sokol/src/main/java/io/khasang/sokol/controller/AppController.java
+++ sokol/src/main/java/io/khasang/sokol/controller/AppController.java
@@ -3,13 +3,17 @@ package io.khasang.sokol.controller;
 import io.khasang.sokol.model.Request;
 import io.khasang.sokol.repository.RequestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
 
@@ -53,6 +57,18 @@ public class AppController {
     public String error403() {
         return "/error/403";
     }
+
+    @GetMapping(value="/logout")
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/login?logout";
+    }
+
+
+
 }
 
 
